9f3d9d8e8ee6b611bbb8cb3f1fdd42b7e2a2787d
patrickishaf <patrickishaf@gmail.com>
2022-07-11 23:38:40 +0100

    optimize the filtration process in some of the controller functions

--- src/modules/posts/middleware/posts-controller.ts
+++ src/modules/posts/middleware/posts-controller.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import SuccessResponse from '../../../app/models/success-response';
-import ErrorResponse from '../../../app/models/error-response';
+import SuccessResponse, { SuccessResponseJSON } from '../../../app/models/success-response';
+import ErrorResponse, { ErrorResponseJSON } from '../../../app/models/error-response';
 import posts from '../services/posts';
 import { validateNewPostObject, validatePostID } from './validators';
 import users from '../../users/services/users';
@@ -12,27 +12,31 @@ export const getPosts = (req: express.Request, res: express.Response) => {
 
 export const getPostWithID = (req: express.Request, res: express.Response) => {
     if (validatePostID(parseInt(req.params.id))) {
-        const result = posts.filter((post) => post.id === parseInt(req.params.id));
-        res.status(200).send(JSON.stringify(SuccessResponse(result)));
+        const result = posts.find((post) => post.id === parseInt(req.params.id));
+        res.status(200).send(SuccessResponseJSON(result));
     } else {
-        res.status(404).send(JSON.stringify(ErrorResponse(404, 'Post Not Found')));
+        res.status(404).send(ErrorResponseJSON(404, 'Post Not Found'));
     }
 }
 
 export const getPostAuthor = (req: express.Request, res: express.Response) => {
     if (validatePostID(parseInt(req.params.id))) {
-        const matchingPost = posts.filter((post) => post.id === parseInt(req.params.id))[0];
-        const author = users.filter((user) => user.id === matchingPost.author.id);
-        res.status(200).send(JSON.stringify(SuccessResponse(author)));
+        const matchingPost = posts.find((post) => post.id === parseInt(req.params.id));
+        if (matchingPost === undefined || matchingPost === null) {
+            res.status(404).send(ErrorResponseJSON(404, 'Post Not Found'));
+        } else {
+            const author = users.filter((user) => user.id === matchingPost.author.id);
+            res.status(200).send(SuccessResponseJSON(author));
+        }
     } else {
-        res.status(404).send(JSON.stringify(ErrorResponse(404, 'Post Not Found')));
+        res.status(404).send(ErrorResponseJSON(404, 'Post Not Found'));
     }
 }
 
 export const createNewPost = (req: express.Request, res: express.Response) => {
     if (validateNewPostObject(req.body) && !validatePostID(req.body.id)) {
         posts.push(req.body);
-        res.status(200).send(JSON.stringify(SuccessResponse(req.body)));
+        res.status(200).send(SuccessResponseJSON(req.body));
     } else {
         res.status(401).send('Error: Bad request');
     }
@@ -40,40 +44,21 @@ export const createNewPost = (req: express.Request, res: express.Response) => {
 
 export const editPost = (req: express.Request, res: express.Response) => {
     try {
-        let matchingPost = posts.filter((post) => post.id === parseInt(req.params.id))[0];
-        let updatedPostData = req.body as Post;
-        if (updatedPostData.id) {
-            matchingPost.id = updatedPostData.id;
-        }
-        if (updatedPostData.title) {
-            matchingPost.title = updatedPostData.title;
-        }
-        if (updatedPostData.id) {
-            matchingPost.id = updatedPostData.id;
-        }
-        if (updatedPostData.body) {
-            matchingPost.body = updatedPostData.body;
-        }
-        if (updatedPostData.author) {
-            matchingPost.author = updatedPostData.author;
-        }
-        if (updatedPostData.timeCreated) {
-            matchingPost.timeCreated = updatedPostData.timeCreated;
-        }
-        posts[matchingPost.id] = matchingPost;
-        res.status(200).send(JSON.stringify(SuccessResponse(matchingPost)));
+        let matchingPost = posts.find((post) => post.id === parseInt(req.params.id)) as Post;
+        let updatedPostData = req.body;
+        const editedPost = Object.assign(matchingPost, updatedPostData, {id: req.params.id});
+        posts[editedPost.id] = editedPost;
+        res.status(200).send(SuccessResponseJSON(editedPost));
     } catch (err) {
-        const error = err as Error;
-        res.status(501).send(JSON.stringify(ErrorResponse(501, 'Not your fault, bro. Server malfunction')));
+        res.status(501).send(ErrorResponseJSON(501, 'Not your fault, bro. Server malfunction'));
     }
 }
 
 export const deletePost = (req: express.Request, res: express.Response) => {
     try {
         const deletedPost = posts.splice((parseInt(req.params.id) - 1), 1);
-        res.status(200).send(JSON.stringify(SuccessResponse(deletedPost)));
+        res.status(200).send(SuccessResponseJSON(deletedPost));
     } catch (err) {
-        const error = err as Error;
-        res.status(501).send(JSON.stringify(ErrorResponse(501, 'Gimme a hug bro. It\'s not your fault, bro. Server malfunction')));
+        res.status(501).send(ErrorResponseJSON(501, 'Gimme a hug bro. It\'s not your fault, bro. Server malfunction'));
     }
 }
