29ecd98fa7edfcd9b8e6463afd58a6ad8f2cbb0e
patrickishaf <patrickishaf@gmail.com>
2022-07-29 13:28:52 +0100

    configure project for heroku deployment

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,6 @@ app.get('/', (req: express.Request, res: express.Response) => {
     res.send('ENTER THE DRAGON');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('ENTER THE DRAGON');
 });
