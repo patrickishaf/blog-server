b1e4c39b5ddd93157951960934ba2e44ea0b9051
patrickishaf <patrickishaf@gmail.com>
2022-08-11 12:16:57 +0100

    base client auth middleware on session id

--- src/modules/auth/middleware/client-auth.ts
+++ src/modules/auth/middleware/client-auth.ts
@@ -3,9 +3,10 @@ import ErrorResponse from '../../../app/response-types/error-response';
 
 const ensureClientAuth = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const session = req.session as any;
-    if (!session.user) {
+    if (!session.id) {
         res.status(501).json(ErrorResponse(501, 'user is not authenticated'));
     } else {
+        console.log('THE SESSION ID IS:', session.id);
         next();
     }
 }
