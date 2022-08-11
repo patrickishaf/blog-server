8001811314d5f79c2e6d41d17408699e7f7afae3
patrickishaf <patrickishaf@gmail.com>
2022-08-11 12:19:54 +0100

    remove the unnecessary post route

--- src/modules/posts/routes/router.ts
+++ src/modules/posts/routes/router.ts
@@ -1,15 +1,16 @@
 import express from 'express';
+import ensureClientAuth from '../../auth/middleware/client-auth';
 import { createNewPost, deletePost, editPost, getPostAuthor, getPosts, getPostWithID } from '../middleware/posts-controller';
 
 const postsRouter:express.Router = express.Router();
+
+postsRouter.use(ensureClientAuth);
 
 postsRouter.get('', getPosts);
 
 postsRouter.get('/:id', getPostWithID);
 
 postsRouter.get('/:id/author', getPostAuthor);
-
-postsRouter.post('', createNewPost);
 
 postsRouter.post('/new', createNewPost);
 
